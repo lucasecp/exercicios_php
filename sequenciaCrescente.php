65dacf395e7a6b937b4aa8361e608de6f644c602
lucas <lucasecp7@gmail.com>
2021-02-16 00:28:56 -0300

    fix

--- sequenciaCrescente.php
+++ sequenciaCrescente.php
@@ -4,17 +4,16 @@
 
       for($i = 0; $i < sizeof($array);$i++){
        $qtdNumRemovido = 0;
-       if($qtdNumRemovido > )
+       if($qtdNumRemovido > 0 ) {$resposta = false;};
        if($array[$i] < $array[$i + 1] || count($array) === 1) {
            $resposta = true;
         }
           else{ 
-              $resposta = true;
-              $qtdNumRemovido = 1
+              $qtdNumRemovido = 1;
             }
       }
      echo $resposta ? 'TRUE': 'FALSE';
    }
-   SequenciaCrescente([10,9]);
+   SequenciaCrescente([9,10,11,3,2]);
 
 ?>
